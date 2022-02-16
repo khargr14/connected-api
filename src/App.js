73557e19caab34ac47356f24bee22433dbcf2af4
khargr14 <kay_hargrove@yahoo.com>
2022-02-15 20:11:54 -0500

    setting up the useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, {useState, useEffect } from "react";
+
 import './App.css';
 
-function App() {
+export default () => {
+  const [count, setCount] = useState(0);
+
+  //Similar to componecntDidMount and componentDidUpate:
+  useEffect(() => {
+    //Update the document title using the brower API
+    document.title = `You clicked ${count} times`;
+ });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        <p>You Clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+      </p>
     </div>
   );
-}
-
-export default App;
+};
